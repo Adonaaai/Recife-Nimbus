5fb1576da07a605388ba76b7fec88be489bfd4ec
adonai artur <adonaiartur852@gmail.com>
2026-05-17 01:15:27 -0300

    Added: monitorJob()

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import 'dotenv/config';
 import { startTelegramBot } from './bot/telegramBot.ts';
+import { monitorJob } from './cron/floodMonitoring.ts';
 
 async function main() {
     try {
-        // Monitor
-        //monitorJob();
-
         // Bot
         startTelegramBot();
+
+        // Monitor
+        await monitorJob();
 
     } catch (err) {
         console.error('Fatal error starting the application:', err);
