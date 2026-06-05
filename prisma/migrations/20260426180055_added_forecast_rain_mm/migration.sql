0d7e549dd089b736fad472c5169821a88c1b9d21
adonai artur <adonaiartur852@gmail.com>
2026-06-05 19:20:52 -0300

    chore: squash migrations into a clean init file

--- prisma/migrations/20260426180055_added_forecast_rain_mm/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "AlertLog" ADD COLUMN     "forecastRainMm" DOUBLE PRECISION;
--- prisma/migrations/20260509170657_add_forecast_tide_river_tendencia_is_active/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "AlertLog" ADD COLUMN     "forecastTide" DOUBLE PRECISION,
-ADD COLUMN     "riverTendencia" TEXT;
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "isActive" BOOLEAN NOT NULL DEFAULT true;
